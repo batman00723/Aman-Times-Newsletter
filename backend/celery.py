a1161a871eda7e5af0e7ac005776be7601a0c0c5
Aman Mishra <batmanmishra23@gmail.com>
2026-06-11 19:35:11 +0530

    Remove unused celery dependencies

--- backend/celery.py
+++ backend/celery.py
@@ -1,12 +1,12 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-app = Celery('backend')
+# app = Celery('backend')
 
-# The namespace='CELERY' tells it to look for CELERY_ in settings.py
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# # The namespace='CELERY' tells it to look for CELERY_ in settings.py
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# This searches your apps for tasks.py files
-app.autodiscover_tasks()
+# # This searches your apps for tasks.py files
+# app.autodiscover_tasks()
